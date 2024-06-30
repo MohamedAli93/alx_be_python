26c4d1ca8aa7a761e2cce13da53f9259ec1574ca
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-06-30 20:27:21 +0300

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,9 @@
-weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
-if weather == "sunny":
+c_weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
+if c_weather == "sunny":
     print ("Wear a t-shirt and sunglasses.")
-elif weather == "rainy": print ("Don't forget your umbrella and a raincoat.")
-elif weather == "cold": print ("Make sure to wear a warm coat and a scarf.")
-else: print ("Sorry, I don't have recommendations for this weather.")
+elif c_weather == "rainy":
+    print ("Don't forget your umbrella and a raincoat.")
+elif c_weather == "cold":
+    print ("Make sure to wear a warm coat and a scarf.")
+else:
+    print ("Sorry, I don't have recommendations for this weather.")
