1b6b0a26016263c9f666f70a1b78e26df5578833
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-07-01 00:14:37 +0300

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -14,4 +14,4 @@ if time_bound == "yes":
     reminder += "that requires immediate attention today!"
 elif time_bound == "no":
     reminder += "Consider completing it when you have free time."
-print(reminder)
+print("Reminder:", reminder)
