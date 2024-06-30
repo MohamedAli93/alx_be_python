f73ac598c16068f54cf9f7b9afe287ecd14f22f7
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-06-30 23:44:30 +0300

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,8 +1,20 @@
-task = str(input("Enter your task: "))
-priority = str(input("Priority (high/medium/low): "))
+task = input("Enter your task: ")
+priority = str(input("Priority (high/medium/low): ")).lower()
 time_bound = str(input("Is it time-bound? (yes/no): ")).lower()
-match time_bound:
-    case "yes":
-        print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
-    case "no":
-        print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+reminder1 = f"Reminder: {task} is a {priority} priority task "
+reminder2 = f"Note: {task} is a {priority} priority task "
+if time_bound == "yes":
+    reminder1 += "that requires immediate attention today!"
+    reminder2 += "that requires immediate attention today!"
+elif time_bound == "no":
+    reminder1 += "Consider completing it when you have free time."
+    reminder2 += "Consider completing it when you have free time."
+match priority:
+    case "high":
+        print(reminder1)
+    case "medium":
+        print(reminder2)
+    case "low":
+        print(reminder2)
+    case _:
+        print("Invalid priority level. Reminder not created.")
