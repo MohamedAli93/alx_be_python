0a8f8426e15178a12bbf6edd9359e2b2b5c8b43f
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-07-12 18:14:26 +0300

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,31 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+shopping_list = []
+def main():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item_name1 = input("item name: ")
+            shopping_list.append(item_name1)
+        elif choice == '2':
+            item_name2 = input("item name: ")
+            if item_name2 in shopping_list:
+                shopping_list.remove(item_name2)
+            else:
+                print("item not found")
+        elif choice == '3':
+           print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
