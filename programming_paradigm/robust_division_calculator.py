8709d82ae882266b5481932d15537e1e255e2f73
Mohamed Ali <The_blue_Whale@Windowslive.com>
2024-07-16 23:55:09 +0300

    Division Calculator with Command Line Arguments

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,12 +2,9 @@ def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
         denominator = float(denominator)
-        if denominator == 0:
-            raise ZeroDivisionError("Error: Cannot divide by zero.")
-
         result = numerator / denominator
         return f"The result of the division is {result:.1f}"
     except ZeroDivisionError as e:
-        return e
+        return f"Error: {e}"
     except ValueError:
         return "Error: Please enter numeric values only."
